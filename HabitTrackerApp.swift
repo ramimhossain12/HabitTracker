b87aa605c726ccb1e5ede06f34fb2b20e2514bf5
ramimhossain12 <ramimhossain71.info@gmail.com>
2026-09-20 23:40:06 +0600

    first commit

---
+++ HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Ramim Hossain on 20/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
